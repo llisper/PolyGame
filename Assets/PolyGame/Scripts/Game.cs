43c6c5955b8a407debbd338ccdc3e17623ae47f6
llisperzhang <llisperzhang@gmail.com>
2018-06-23 22:24:19 +0800

    build apk
    1. use auto graphics API
    2. always include wireframesimplify shader

--- Assets/PolyGame/Scripts/Game.cs
+++ Assets/PolyGame/Scripts/Game.cs
@@ -17,4 +17,28 @@ public class Game : MonoBehaviour
         if (null != testPuzzle)
             Puzzle.Start(testPuzzle.name);
 	}
+
+    GUIStyle style;
+    float fps;
+    float fpsTimer;
+    int fpsCounter;
+
+    void OnGUI()
+    {
+        if (null == style)
+        {
+            style = new GUIStyle();
+            style.fontSize = 15;
+            style.normal.textColor = Color.green;
+            fpsCounter = Time.frameCount;
+        }
+
+        if ((fpsTimer += Time.deltaTime) > 1f)
+        {
+            fps = (Time.frameCount - fpsCounter) / fpsTimer;
+            fpsCounter = Time.frameCount;
+            fpsTimer = 0f;
+        }
+        GUI.Label(new Rect(10, 10, 1000, 1000), string.Format("fps: {0:f2}", fps), style);
+    }
 }
