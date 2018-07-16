93620bd5640321f60cc331bb7a383bab02a2622c
llisperzhang <llisperzhang@gmail.com>
2018-07-16 17:55:50 +0800

    case constraint on import

--- Assets/PolyGame/Editor/ImportPostProcessor.cs
+++ Assets/PolyGame/Editor/ImportPostProcessor.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.IO;
+using System.Text.RegularExpressions;
 
 class ImportPostProcessor : AssetPostprocessor
 {
@@ -11,5 +13,25 @@ class ImportPostProcessor : AssetPostprocessor
         importer.isReadable = true;
         importer.mipmapEnabled = false;
         importer.filterMode = FilterMode.Trilinear;
+    }
+
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string path in importedAssets)
+        {
+            if (path.StartsWith(Paths.AssetArtworks))
+            {
+                string filename = Path.GetFileName(path);
+                if (Regex.IsMatch(filename, @"[A-Z]"))
+                {
+                    AssetDatabase.RenameAsset(path, filename.ToLower());
+                    Debug.LogFormat(
+                        "(Case-Check) rename {0} -> {1}/{2}",
+                        path,
+                        Path.GetDirectoryName(path),
+                        filename.ToLower());
+                }
+            }
+        }
     }
 }
