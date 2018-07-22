7111e8e253d8f9949ccedd4d0fe414da9fc96f4d
llisperzhang <llisperzhang@gmail.com>
2018-07-22 16:30:45 +0800

    build script

--- Build/Scripts/build.py
+++ Build/Scripts/build.py
@@ -10,17 +10,8 @@ builders = {
     'iOS' : None,
 }
 
-p = subprocess.Popen('git status -s'.split(), stdout=subprocess.PIPE)
-changes = p.communicate()[0]
-if len(changes) > 0:
-    print('there are local changes that has not been commited, commit or cleanup the workspace first.')
-    sys.exit(1)
-
 env = env.init_env()
 args = argparser.parse()
 print(args)
 builders[args.buildTarget](env, args)
-
-subprocess.check_call('git reset --hard HEAD')
-subprocess.check_call('git clean --fd')
 
--- Build/local_build_android.sh
+++ Build/local_build_android.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
 root=`dirname $0`
+cd $root
+changes=`git status -s`
+if [[ ! -z "${changes// }" ]] 
+then
+    read -p "There are uncommited changes, continue will clean local changes. Continue?[y/n]" respond
+    if [[ "$respond" = "Y" ]] || [[ "$respond" == "y" ]]
+    then
+        echo "CLEANUP WORKSPACE..."
+        git reset --hard HEAD
+        git clean -fd
+    else
+        exit 1
+    fi
+fi
+
 cd $root/Scripts
+echo "INVOKE BUILD SCRIPT..."
 python -B build.py -clean -buildTarget Android -buildFlags Resource Bundle -cdn "skip-update"
+status=$?
+if [[ $status -ne 0 ]]
+then
+    echo "BUILD FAILED WTIH $status" >&2
+fi
+
+echo "CLEANUP WORKSPACE..."
+sleep 1
+git reset --hard HEAD
+git clean -fd
 
 cd $root
+echo "COPY PACKAGE..."
+sleep 1
 cp Output/Android/PolyGame/build/outputs/apk/*.apk Output/Android
